0c57710031e27019e27c174517b94bb0e7f485ce
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-06-03 16:17:03 +0530

    Camera Permissions Handled

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,15 +1,41 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 function ImagePicker() {
-  async function takeImageHandler() {
-    const image = await launchCameraAsync(
-        {
-            allowsEditing:true,
-            aspect:[16,9],
-            quality:0.5
-        }
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert('Insufficient Permissions!',
+      'You need to grant camera permissions to us the app.'
     );
+      return false;
+    }
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission =  await verifyPermissions();
+
+if(!hasPermission){
+return;
+}
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
     console.log(image);
   }
   return (
